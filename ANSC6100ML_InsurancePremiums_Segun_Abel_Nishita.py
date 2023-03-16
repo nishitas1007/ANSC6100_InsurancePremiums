275d38615431cb8ce0703f8da38a1114c810f2a8
nishitas1007 <nishita.sharif@gmail.com>
2023-03-15 20:50:44 -0400

    ethleaiuabgjk

--- ANSC6100ML_InsurancePremiums_Segun_Abel_Nishita.py
+++ ANSC6100ML_InsurancePremiums_Segun_Abel_Nishita.py
@@ -5,12 +5,12 @@ import pandas
 import io
 
 # GitHub URL to .csv
-dataset_url_path = 'https://raw.githubusercontent.com/nishitas1007/ANSC6100_InsurancePremiumML/master/insurance_dataset.csv'
+dataset_url_path = 'https://raw.githubusercontent.com/nishitas1007/ANSC6100_InsurancePremiums/main/insurance_dataset.csv'
 
 # Assigning the .csv in the URL to a dataframe 
 insurance_premium_df = pandas.read_csv(dataset_url_path)
 
-# use the info() function to see information about our data frame
+# Use the info() function to see information about our data frame
 insurance_premium_df.info()
 # There are no "null" values in any of the 7 columns of our data frame – so we don't need to remove any NAs
 
